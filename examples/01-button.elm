9db721c3b230c275c039b968d4ecf47838f4d1ef
Andrew MacDonald <awm@alum.mit.edu>
2016-11-29 18:42:57 -0800

    Put + button on top and - on bottom. Makes more sense

--- examples/01-button.elm
+++ examples/01-button.elm
@@ -49,7 +49,7 @@ update msg model =
 view : Model -> Html Msg
 view model =
   div []
-    [ button [ onClick Decrement ] [ text "-" ]
+    [ button [ onClick Increment ] [ text "+" ]
     , div [] [ text (toString model) ]
-    , button [ onClick Increment ] [ text "+" ]
+    , button [ onClick Decrement ] [ text "-" ]
     ]
